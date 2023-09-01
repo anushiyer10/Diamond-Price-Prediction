9149fcb2503706184babddb1c9bbe934ed9879f6
Anush <anushiyer10@gmail.com>
2023-09-01 12:44:35 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPHON_E_DOT = '-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+        if HYPHON_E_DOT in requirements:
+            requirements.remove(HYPHON_E_DOT)
+        return requirements
+
+setup(
+    name = 'Diamond Price Prediction',
+    version = '0.0.1',
+    author = 'Anush' ,
+    author_email = 'anushiyer10@gmail.com' ,
+    install_requires = get_requirements('requirement.txt'),
+    packages = find_packages()
+)
